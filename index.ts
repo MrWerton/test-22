d2a0a2b9b0f601244577ca33f9fb4e40ec702e88
werton <mr.werton@gmail.com>
2024-05-07 23:20:12 -0300

    .

--- index.ts
+++ index.ts
@@ -1,15 +1,14 @@
-import express from 'express'
+import express from 'express';
 
+const app = express();
+app.use(express.json());
 
-const app = express()
+app.get('/', (req, res) => {
+   return res.json('Hello World');
+});
 
-
-app.get('/`', (req, res) => {
-    res.send('Hello World')
-})
-
-const PORT = process.env.PORT || 5000
+const PORT =  5444;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-})
+    console.log(`Server is running on port http://localhost:${PORT}`);
+});
